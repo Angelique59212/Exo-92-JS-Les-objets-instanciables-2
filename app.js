4937778b6efbb9c56e48a9155e06ff0dd27a7b07
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2021-12-03 11:21:16 +0100

    bonus

--- app.js
+++ app.js
@@ -11,18 +11,18 @@ const Person = function (name, firstName,age,sex,hobbies) {
     }
 }
 
-let array = [];
-
 let angelique = new Person("Dehainaut", "Angélique", 33, "féminin", ['l/écriture', 'le chant', 'la musique']);
 let louane = new Person('Laurent', 'Louane', 9,'féminin', ['la boxe', 'les chevaux', 'la musique']);
 let luka = new Person("Laurent", "Luka", 10, "masculin", ["la boxe", "la canne de combat", "les jeux vidéos"]);
 let mickael = new Person("Laurent", "Mickael", 32, "masculin", ["les pompiers", "le sport"]);
 let timeo = new Person("Laurent", "Timeo", 5, "masculin", ["la boxe", "la musique", "l'escalade"]);
 
-array.push(angelique, louane, luka, mickael, timeo);
+let  Classe = {
+    nom : [angelique, louane, luka, mickael, timeo],
+}
 
-for (let i = 0; i < array.length; i++) {
+for (let i = 0; i < Classe.nom.length; i++) {
     let div = document.createElement("div");
-    div.innerHTML = array[i].getPerson();
+    div.innerHTML = Classe.nom[i].getPerson();
     document.body.appendChild(div);
 }
